b7d3a7bd0469041f86585ae08bb34e59479c0adc
Aras Yildiz <ay1218@graphic02.doc.ic.ac.uk>
2019-07-25 20:02:17 +0100

    changes in cc gan plott

--- code/unsupervised_training/semi_supervised_cc_gan.py
+++ code/unsupervised_training/semi_supervised_cc_gan.py
@@ -9,12 +9,12 @@ import pandas as pd
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
-import torchvision.utils as vutils
 #MODELS
 import re
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
+
 import matplotlib.pyplot as plt
 import torchvision.utils as vutils
 import matplotlib.animation as animation
@@ -60,7 +60,7 @@ print(device)
 def train_ccgan(method="CC_GAN",resize = 320, num_epochs=3, lr=0.0002, batch_size=16,noised=1, from_checkpoint=None, from_Pretrained = None ,root_PATH = root_PATH ,root_PATH_dataset=root_PATH_dataset, saved_model_PATH=saved_model_PATH, show=False):
 
     kwargs_cc_gan_patch ={"show":show, "gpu" : use_cuda}
-    kwarg_Common ={"num_epochs":num_epochs,"learning_rate":lr,"batch_size":batch_size}
+    kwarg_Common ={"num_epochs":num_epochs,"learning_rate":lr,"batch_size":batch_size,"resize":resize}
     kwargs={"Common":kwarg_Common}
 
     #just ToTensor before patch
@@ -81,9 +81,8 @@ def train_ccgan(method="CC_GAN",resize = 320, num_epochs=3, lr=0.0002, batch_siz
     _, valid_dataloader = chexpert_load.chexpert_load(root_PATH + "SummerThesis/code/custom_lib/chexpert_load/valid.csv",
                                                                                  transform_train,8, labels_path=labels_path,root_dir = root_PATH_dataset,shuffle = False)
 
-    fixed_noise = torch.randn(batch_size,100,1, 1).to(device = device)
+    fixed_noise = torch.randn(8,100,1, 1).to(device = device)
     iter_fixedvalid =iter(valid_dataloader)
-
     valid_batch = next(iter_fixedvalid)[0]
     valid_batch2 = next(iter_fixedvalid)[0]
 
@@ -94,18 +93,6 @@ def train_ccgan(method="CC_GAN",resize = 320, num_epochs=3, lr=0.0002, batch_siz
     fixed_context_conditioned2,fixed_low_res2 ,fixed_cord2 = patcher2()
 
 
-
-    # Plot some training images
-    '''
-    real_batch = next(iter(dataloader))
-    real_batch2 = next(iter(dataloader))
-
-    plt.figure(figsize=(8,4))
-    plt.axis("off")
-    plt.title("Training Images")
-    plt.imshow(np.transpose(vutils.make_grid(torch.cat((real_batch[0].to(device)[:64],real_batch2[0].to(device)[:64])), padding=2, normalize=True).cpu(),(1,2,0)))
-    plt.show()
-    '''
 
     netD = cc_gan.Discriminator()
     netG = cc_gan.Generator(noise=noised,noise_k_size=int(resize/16))
@@ -126,21 +113,21 @@ def train_ccgan(method="CC_GAN",resize = 320, num_epochs=3, lr=0.0002, batch_siz
 
 
     if from_Pretrained :
-        loader = load_model.Load_Model(method=method,pre_trained = pre_trained_PATH, kwargs=kwargs, model=[netG,netD], optimizer=[optimizerG,optimizerD], plot_loss=[G_losses,D_losses] )
+        loader = load_model.Load_Model(method=method,pre_trained = pre_trained_PATH, kwargs=kwargs, model=[netG,netD], optimizer=[optimizerG,optimizerD], plot_loss=[G_losses,D_losses,C_loss] )
         file_name  ,plot_loss  = loader()
 
     elif from_checkpoint :
-        loader = load_model.Load_Model(method=method ,from_checkpoint = from_checkpoint,  kwargs=kwargs, model=[netG,netD], optimizer=[optimizerG,optimizerD], plot_loss=[G_losses,D_losses] )
+        loader = load_model.Load_Model(method=method ,from_checkpoint = from_checkpoint,  kwargs=kwargs, model=[netG,netD], optimizer=[optimizerG,optimizerD], plot_loss=[G_losses,D_losses,C_loss] )
         file_name ,plot_loss  = loader()
 
     else:
         print("training GAN from scratch")
-        file_name=method +"_"+ "".join([key + str(kwargs["Common"][key]) for key in kwargs["Common"].keys()])+".tar"
+        file_name=method + "".join(["_"+key + str(kwargs["Common"][key]) for key in kwargs["Common"].keys()])+".tar"
 
 
     saved_model_PATH = saved_model_PATH+  "saved_models/semi_supervised/"+ file_name[:-4]
     if not os.path.exists(saved_model_PATH): os.mkdir(saved_model_PATH)
-
+    log_file = open(saved_model_PATH+"/log_file.txt","a")
     img_list = []
     c_loss = None
 
@@ -149,11 +136,13 @@ def train_ccgan(method="CC_GAN",resize = 320, num_epochs=3, lr=0.0002, batch_siz
     netD.to(device)
     netG.to(device)
     print("Starting Training Loop...")
+    print(file_name)
     # For each epoch
     for epoch in range(num_epochs):
         # For each batch in the dataloader
         for i, (real_images , class_labels , label_ok) in enumerate(dataloader):
 
+            bs = real_images.shape[0]
             real_images = real_images.to(device=device,dtype=torch.float)
             # Real img to 3 channel for D and patcher for G
             patcher = cc_gan.Patcher_CC_GAN(real_images,**kwargs_cc_gan_patch)#, transform = transform_after_patching)
@@ -166,7 +155,7 @@ def train_ccgan(method="CC_GAN",resize = 320, num_epochs=3, lr=0.0002, batch_siz
             ## Train with all-real batch
             netD.zero_grad()
             # Format batch
-            label = torch.full((batch_size,), real_label, device=device)
+            label = torch.full((bs,), real_label, device=device)
             # Forward pass real batch through D
             output = netD(real_images)
             # Calculate loss on all-real batch
@@ -189,25 +178,68 @@ def train_ccgan(method="CC_GAN",resize = 320, num_epochs=3, lr=0.0002, batch_siz
 
             ## Train with all-fake batch
             # Generate batch of latent vectors (context conditioned in our case)
-            context_conditioned,low_res ,cord = patcher()
+            context_conditioned, low_res ,cord = patcher()
+            context_conditioned = context_conditioned.to(device=device)
+            low_res = low_res.to(device=device)
+
             # Generate fake image batch with G
-            noise = torch.randn(batch_size,100,1, 1).to(device=device)
+            noise = torch.randn(bs,100,1, 1).to(device=device)
             fake = netG(context_conditioned,low_res,cord,noise)
             label.fill_(fake_label)
 
             '''PROBLEEEEEEm'''
+            filled =context_conditioned.clone()
+            filled_fake =context_conditioned.clone()
+
             hole_size = cord[0]
             for idx , f_i in enumerate(fake):
+
                 mask = torch.zeros(resize,resize).to(device=device)
                 row  = cord[1][idx][0]
                 col  = cord[1][idx][1]
                 mask[row:row+hole_size,col:col+hole_size]=1
-                #fake[idx].data = fake[idx]*mask+(1-mask)*context_conditioned[idx]
-                fake[idx].data = torch.where(mask.byte(),fake[idx],context_conditioned[idx])
 
-            fake = torch.stack([channel3(f_im) for f_im in fake ])
+                filled[idx] = torch.where(mask.byte(),fake[idx].clone(),context_conditioned[idx])
+
+            #
+            # if i % 50 == 0:
+            #      #plt.figure("epoch:"+str(epoch)+"iteration_number: "+str(i))
+            #     plt.ion()
+            #
+            #     plt.figure("1")
+            #     plt.imshow(fake[0].detach().cpu().squeeze().numpy(),cmap='Greys_r')
+            #     plt.title("epoch:"+str(epoch)+" iteration_number: "+str(i))
+            #     plt.draw()
+            #
+            #     plt.pause(0.1)
+            #     plt.savefig(root_PATH+ str(i)+"fakeG.png")
+            #
+            #     plt.figure("2")
+            #     plt.imshow(real_images[0,0].detach().cpu().squeeze().numpy(),cmap='Greys_r')
+            #     plt.title("epoch:"+str(epoch)+" iteration_number: "+str(i))
+            #     plt.draw()
+            #
+            #     plt.pause(0.1)
+            #     plt.savefig(root_PATH+str(i)+"real.png")
+            #
+            #     plt.figure("3")
+            #
+            #     plt.imshow(filled[0].detach().cpu().squeeze().numpy(),cmap='Greys_r')
+            #     plt.title("epoch:"+str(epoch)+" iteration_number: "+str(i))
+            #
+            #     plt.draw()
+            #     plt.pause(0.1)
+            #     # at the end call show to ensure window won't close.
+            #     #print('continue computation')
+            #     plt.savefig(root_PATH+str(i)+"filled.png")
+
+
+
+            filled = torch.stack([channel3(f_im) for f_im in filled ])
             # Classify all fake batch with D (din't forget to detach because you just optimize discrimantor)
-            output = netD(fake.detach())
+            output = netD(filled.detach())
+            #output = netD(fake.detach())
+
             # Calculate D's loss on the all-fake batch
             errD_fake =  advs_criterion( sig(output[:, 0].view(-1)), label)
             # Calculate the gradients for this batch
@@ -226,7 +258,9 @@ def train_ccgan(method="CC_GAN",resize = 320, num_epochs=3, lr=0.0002, batch_siz
             netG.zero_grad()
             label.fill_(real_label)  # fake labels are real for generator cost
             # Since we just updated D, perform another forward pass of all-fake batch through D
-            output = netD(fake)
+            output = netD(filled)
+            #output = netD(fake)
+
             # Calculate G's loss based on this output
             errG = advs_criterion(sig(output[:, 0].view(-1)), label) #+ classification_criterion(output[1:], class_labels)
             # Calculate gradients for G
@@ -239,14 +273,20 @@ def train_ccgan(method="CC_GAN",resize = 320, num_epochs=3, lr=0.0002, batch_siz
             #print("breaking");break
             # Output training stats
             if (i+1) % 100 == 0:
+                 print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                      % (epoch+1, num_epochs, i+1, len(dataloader),
+                         errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+
+                 log_file.write('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                      % (epoch+1, num_epochs, i+1, len(dataloader),
+                         errD.item(), errG.item(), D_x, D_G_z1, D_G_z2) + "\n")
+
+
                  aftertDT = datetime.datetime.now()
                  c = aftertDT-currentDT
                  mins,sec=divmod(c.days * 86400 + c.seconds, 60)
                  print(mins,"mins ", sec,"secs")
 
-                 print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
-                      % (epoch+1, num_epochs, i+1, len(dataloader),
-                         errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
 
             if (i+1) % 200 == 0:
                 # Save Losses for plotting later
@@ -258,39 +298,43 @@ def train_ccgan(method="CC_GAN",resize = 320, num_epochs=3, lr=0.0002, batch_siz
 
 
             # Check how the generator is doing by saving G's output on fixed_noise
-            if (i % 6000 == 0) or((epoch == num_epochs-1) and (i == len(dataloader)-1)):
+            if (i % 4000 == 0) or((epoch == num_epochs-1) and (i == len(dataloader)-1)):
 
                 # Generate fake image batch with G
                 with torch.no_grad():
-
-                    netG.eval()
                     fake = netG(fixed_context_conditioned,fixed_low_res,fixed_cord,fixed_noise).detach().cpu()
                     fake2 = netG(fixed_context_conditioned2,fixed_low_res2,fixed_cord2,fixed_noise).detach().cpu()
-                    netG.train()
-                    Gen_fake = fake.detach().clone().cpu()
-                    Gen_fake2 = fake2.detach().clone().cpu()
 
                 hole_size = fixed_cord[0]
                 hole_size2 = fixed_cord2[0]
 
-                for idx , f_i in enumerate(fake):
-                    mask=torch.zeros(1,resize,resize)
-                    mask2=torch.zeros(1,resize,resize)
+                fixed_filled = fixed_context_conditioned.clone().cpu()
+                fixed_filled2 = fixed_context_conditioned2.clone().cpu()
+
+                fixed_context_conditioned =  fixed_context_conditioned.cpu()
+                fixed_context_conditioned2 = fixed_context_conditioned2.cpu()
+
+                for idx , _ in enumerate(fake2):
+
+                    mask = torch.zeros(resize,resize).cpu()
+                    mask2 = torch.zeros(resize,resize).cpu()
 
                     row = fixed_cord[1][idx][0];row2 = fixed_cord2[1][idx][0]
                     col = fixed_cord[1][idx][1];col2 = fixed_cord2[1][idx][1]
-                    mask[:,row:row+hole_size,col:col+hole_size]=1
-                    mask2[:,row2:row2+hole_size2,col2:col2+hole_size2]=1
+
+                    mask[row:row+hole_size,col:col+hole_size] = 1.0
+                    mask2[row2:row2+hole_size2,col2:col2+hole_size2] = 1.0
+
+                    fixed_filled[idx] = torch.where(mask.byte().cpu(),fake[idx].clone().cpu(),fixed_context_conditioned[idx].cpu())
+                    fixed_filled2[idx] = torch.where(mask2.byte(),fake2[idx].clone(),fixed_context_conditioned2[idx])
 
 
-                    fake[idx] = torch.where(mask.byte(),fake[idx],fixed_context_conditioned[idx].cpu())
-                    fake2[idx] = torch.where(mask2.byte(),fake2[idx],fixed_context_conditioned2[idx].cpu())
+                grid=vutils.make_grid(torch.cat((valid_batch,fixed_context_conditioned.cpu(),fake.cpu(),fixed_filled,
+                                                        valid_batch2,fixed_context_conditioned2.cpu(), fake2.cpu(),fixed_filled2)).cpu(), padding=2, normalize=True,nrow=8)[0]
+                img_list.append(grid)
 
-#                    fake[idx].data = fake[idx]*mask+(1-mask)*context_conditioned[idx]
-#                    fake2[idx].data = fake2[idx]*mask2+(1-mask2)*context_conditioned2[idx]
 
-                img_list.append(vutils.make_grid(torch.cat((valid_batch,fixed_context_conditioned.cpu(),Gen_fake,fake,
-                                                            valid_batch2,fixed_context_conditioned2.cpu(), Gen_fake2,fake2)).cpu(), padding=2, normalize=True))
+
 
     print('training done')
     aftertDT = datetime.datetime.now()
@@ -318,19 +362,12 @@ def train_ccgan(method="CC_GAN",resize = 320, num_epochs=3, lr=0.0002, batch_siz
     plt.savefig(saved_model_PATH+'/'+'plot_loss_'+ '.png')
 
     Writer = animation.writers["ffmpeg"]
+
     fig, ax = plt.subplots(1,figsize=(64,64))
     plt.axis("off")
-    '''
-    ax[0].set_ylabel('Orginal Image');ax[0].set_ylabel('Cropped Image');ax[0].set_ylabel('Generated Image');ax[0].set_ylabel('Inpainted image')
-    ax[0].set_xlabel('common xlabel')
-    ax[0].set_xlabel('common xlabel')
-    ax[0].set_xlabel('common xlabel')
-    '''
-    ims = [[plt.imshow(np.transpose(img.numpy(),(1,2,0)), animated=True)] for img in img_list]
-    ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
-    #ims2 = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
-    #ani2 = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
 
+    ims = [[plt.imshow(img.numpy(), animated=True,cmap='Greys_r')] for img in img_list]
+    ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
     ani.save(saved_model_PATH+"/G_during_training.mp4")
 
     torch.save({
@@ -349,13 +386,19 @@ def train_ccgan(method="CC_GAN",resize = 320, num_epochs=3, lr=0.0002, batch_siz
     head_state_list = [head["head"].state_dict()  for head in head_arch]
     optimizer_state_list=[head['optimizer'].state_dict()  for head in head_arch]
     '''
-    #torch.save(model.state_dict(), PATH)
+    file1.close()
     print('saved  model(model,optim,loss, epoch)')
 
 #'''(method="relative_position",num_epochs=3, learning_rate=0.0001, batch_size=16,split = 3.0, grid_crop_size=225,patch_crop_size=64,perm_set_size=300)'''
 
 p = saved_model_PATH +'saved_models/semi_supervised/'
-schedule=[ {"num_epochs":3,"show":False, "resize":128,"batch_size":32}]
+
+schedule=[
+            {"num_epochs":3,"show":False, "resize":128,"batch_size":64},
+            {"num_epochs":3,"show":False, "resize":128,"batch_size":32},
+            {"num_epochs":3,"show":False, "resize":128,"batch_size":16},
+            {"num_epochs":3,"show":False, "resize":320,"batch_size":16}
+            ]
 
 
 for kwargs in schedule:
